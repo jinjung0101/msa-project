4679937aa816ed330a8a7ea3eee4d0d09065843c
jinjung0101 <qwe5344641@gmail.com>
2025-05-15 22:25:49 +0900

    base.entity 네이밍 수정

--- apps/event/src/infrastructure/database.module.ts
+++ apps/event/src/infrastructure/database.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SharedMongooseModule } from 'infrastructure/mongoose.module';
 
-import { ConditionBaseSchema }            from '../adapters/persistence/schemas/condition/base.entity';
+import { ConditionBaseSchema }            from '../adapters/persistence/schemas/condition/condition.base.entity';
 import { ConditionDiscriminatorProvider } from '../adapters/persistence/schemas/condition/condition.discriminator';
 
-import { RewardDefinitionBaseSchema }            from '../adapters/persistence/schemas/reward-definition/base.entity';
+import { RewardDefinitionBaseSchema }            from '../adapters/persistence/schemas/reward-definition/reward-definition.base.entity';
 import { RewardDefinitionDiscriminatorProvider } from '../adapters/persistence/schemas/reward-definition/reward-definition.discriminator';
 
 import { EventEntity, EventSchema }           from '../adapters/persistence/schemas/event.entity';
